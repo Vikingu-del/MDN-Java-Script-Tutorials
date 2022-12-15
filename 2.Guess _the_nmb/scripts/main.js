f230ae39b54db3b81834bfdd054f8264c8a8d44f
Tin <rk.seferi@gmail.com>
2022-12-15 18:49:47 +0100

    build it the functions and variables of the game

--- 2.Guess _the_nmb/scripts/main.js
+++ 2.Guess _the_nmb/scripts/main.js
@@ -1,21 +1,76 @@
-// Function: creates a new paragraph and appends it to the bottom of the HTML body.
+//declaring variables
+let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-function createParagraph() {
-    const para = document.createElement('p');
-    para.textContent = 'You clicked the button!';
-    document.body.appendChild(para);
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+
+//building functions
+function checkGuess() {
+  const userGuess = Number(guessField.value);
+  if (guessCount === 1) {
+    guesses.textContent = 'Previous guesses: ';
   }
-  
-  /*
-    1. Get references to all the buttons on the page in an array format.
-    2. Loop through all the buttons and add a click event listener to each one.
-  
-    When any button is pressed, the createParagraph() function will be run.
-  */
-  
-  const buttons = document.querySelectorAll('button');
-  
-  for (const button of buttons) {
-    button.addEventListener('click', createParagraph);
+  guesses.textContent += `${userGuess} `;
+
+  if (userGuess === randomNumber) {
+    lastResult.textContent = 'Congratulations! You got it right!';
+    lastResult.style.backgroundColor = 'green';
+    lowOrHi.textContent = '';
+    setGameOver();
+  } else if (guessCount === 10) {
+    lastResult.textContent = '!!!GAME OVER!!!';
+    lowOrHi.textContent = '';
+    setGameOver();
+  } else {
+    lastResult.textContent = 'Wrong!';
+    lastResult.style.backgroundColor = 'red';
+    if (userGuess < randomNumber) {
+      lowOrHi.textContent = 'Last guess was too low!';
+    } else if (userGuess > randomNumber) {
+      lowOrHi.textContent = 'Last guess was too high!';
+    }
   }
-  
+
+  guessCount++;
+  guessField.value = '';
+  guessField.focus();
+}
+
+guessSubmit.addEventListener('click', checkGuess);
+
+function setGameOver() {
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement('button');
+  resetButton.textContent = 'Start new game';
+  resetButton.style.backgroundColor = 'yellow'
+  document.body.append(resetButton);
+  resetButton.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+  guessCount = 1;
+
+  const resetParas = document.querySelectorAll('.resultParas p');
+  for (const resetPara of resetParas) {
+    resetPara.textContent = '';
+  }
+
+  resetButton.parentNode.removeChild(resetButton);
+
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = '';
+  guessField.focus();
+
+  lastResult.style.backgroundColor = 'white';
+
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+}
